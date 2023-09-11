dd5a76babc121515e2ffb215d6a3561cf619248d
Daniel Xavier <danielcx@gmail.com>
2023-09-11 22:24:24 +0530

    add images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Only for testing Deployment</h1>
+      <img src="https://images.unsplash.com/photo-1682686581663-179efad3cd2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" />
     </div>
   );
 }
